7023ae632229b6357ec306dab71d230ca1f1da9c
Shreyash Thengne <shreyashthengne@gmail.com>
2024-02-04 13:26:24 +0530

    updated

--- src/loggers.py
+++ src/loggers.py
@@ -13,6 +13,3 @@ logging.basicConfig(
     format="hello [ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO,
 )
-
-if __name__  == '__main__':
-    logging.info("Logging started!")
